4693a46aa71adaadfad002bb67e3d3aef327a148
Dhyey Moral <dhyeymoral.dev@gmail.com>
2026-02-03 21:45:38 +0530

    more on Variables

--- 01-Basic-of-JavaScript/script.js
+++ 01-Basic-of-JavaScript/script.js
@@ -1,3 +1,16 @@
 let x = 50;
 let y = 100;
 console.log(x + y);
+
+myName = "Dhyey";
+console.log(myName);
+
+x1 = null;
+console.log(x1);
+x2 = undefined;
+console.log(x2);
+
+b1 = true;
+console.log(b1);
+b2 = false;
+console.log(b2);
