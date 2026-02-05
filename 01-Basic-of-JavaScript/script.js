e95bb27cf9ae486917cd15377bc587246cdef0c3
Dhyey Moral <dhyeymoral.dev@gmail.com>
2026-02-05 16:27:36 +0530

    Datatypes

--- 01-Basic-of-JavaScript/script.js
+++ 01-Basic-of-JavaScript/script.js
@@ -14,3 +14,40 @@ b1 = true;
 console.log(b1);
 b2 = false;
 console.log(b2);
+
+let age = 20;
+console.log(typeof(age));
+
+let username = "Iron Man";
+console.log(typeof(username));
+
+let isfollow = true;
+console.log(typeof(isfollow));
+
+let u;
+console.log(typeof(U));
+
+let n = null;
+console.log(typeof(n));
+
+let bint = BigInt("123456");
+console.log(typeof(bint));
+
+let s = Symbol("^&*)");
+console.log(typeof(s));
+
+const student = {
+    name: "Dhyey",
+    sAge: 20,
+    pass: true,
+    sID: 333
+};
+console.log(student);
+
+console.log(typeof(student));
+
+console.log(student["name"]);
+console.log(student.sAge);
+
+student['sAge'] = student["sAge"] + 1;
+console.log(student.sAge);
