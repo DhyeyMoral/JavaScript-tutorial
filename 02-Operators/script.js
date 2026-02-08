9b30ecc6210176ecfcac97680efd0737732bb166
Dhyey Moral <dhyeymoral.dev@gmail.com>
2026-02-08 22:15:47 +0530

    Assignment Operators

--- 02-Operators/script.js
+++ 02-Operators/script.js
@@ -10,3 +10,14 @@ console.log(num1++);
 console.log(num1--);
 console.log(--num1);
 console.log(++num1);
+
+let num3 = 65;
+let num4 = 31;
+
+console.log(num3 += num4);
+console.log(num3 -= num4);
+console.log(num3 *= num4);
+console.log(num3 /= num4);
+console.log(num3 %= num4);
+console.log(num4 + "Hello");
+
