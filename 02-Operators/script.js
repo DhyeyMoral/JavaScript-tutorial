c1d99684f622271d0d728505e2adeb20cbaf96a6
Dhyey Moral <dhyeymoral.dev@gmail.com>
2026-02-08 22:11:11 +0530

    Arithmetic operators

---
+++ 02-Operators/script.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1++);
+console.log(num1--);
+console.log(--num1);
+console.log(++num1);
